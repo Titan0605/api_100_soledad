e0be316a7acaa6774558ea08638f868bdfd9a3a3
Josue Polanco <josue224yosi@gmail.com>
2025-06-05 16:40:07 -0600

    - Add async request.

--- app/static/javascript/index.js
+++ app/static/javascript/index.js
@@ -2,19 +2,33 @@ const to_search = document.getElementById('to_search');
 const search_btn = document.getElementById('search_btn');
 const checkboxes = document.querySelectorAll('.tag');
 
-search_btn.addEventListener('click', function () {
+search_btn.addEventListener('click', async function () {
     try {
         // gets the text writed in the input
         const valueInput = to_search.value;
-        console.log(valueInput);
         // converts the list of checkboxes in an Array
         const checkboxArray = Array.from(checkboxes)
         // gets by filtrating only the checkboxes checked and for each one get the id
         const checked = checkboxArray.filter(checkboxes => checkboxes.checked).map(checkboxes => checkboxes.id)
 
-        console.log(checked)
+        console.log('To search: ', valueInput)
+        console.log('Checked: ', checked)
+
+        let data = {
+            'to_search': valueInput,
+            'filters': checked
+        }
+
+        if (checked.length > 0 && valueInput.value.length > 0) {
+            const response = await fetch("/search", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(data),
+            });
+        }
     } catch (error) {
         console.log('Error found: ', error)
     }
-
-})
+});
