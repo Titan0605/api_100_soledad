4b678e2b1e0e68a63ccb17b0f9eb1808d6bdfb4c
Edwihn <a6523150063@utch.edu.mx>
2025-06-11 09:28:02 -0600

    Almost finished the front

--- app/static/javascript/drawData.js
+++ app/static/javascript/drawData.js
@@ -14,7 +14,14 @@ export function drawResults(results) {
             case 'events':
                 card.className = 'bg-gray-800 text-white rounded-2xl shadow-lg p-6 flex flex-col gap-3 hover:scale-[1.02] transition-transform duration-200'
                 card.innerHTML = `<div class="mb-3">
-                        <h2 class="text-xl font-bold text-white">${result.nombre}</h2>
+                        <div class="flex justify-between items-center">
+                            <h2 class="text-xl font-bold text-white">${result.nombre}</h2>
+                            <a href="">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                                </svg>
+                            </a>
+                        </div>
                         <div class="flex items-center gap-2 mt-1">
                             <span class="text-sm bg-red-600 text-white px-2 py-1 rounded">Evento</span>                            
                             <span class="text-sm text-gray-300 px-2 py-1 rounded">Capitulo ${result.capitulo}</span>                            
