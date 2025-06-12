6be7971266d7f041425531c074dd14df6e2b5226
Titan0605 <titancisneros@gmail.com>
2025-06-11 18:09:29 -0600

    - Updated forms in modal to match with the general design

--- app/static/javascript/insertService.js
+++ app/static/javascript/insertService.js
@@ -1,16 +1,17 @@
 function mostrarFormulario(id) {
-    ["formEvent", "formCharacter", "formObject", "formLocation"].forEach((f) => {
-        document.getElementById(f).classList.add("hidden");
-    });
-    document.getElementById(id).classList.remove("hidden");
+  // Lista de todos los formularios
+  ["formEvent", "formCharacter", "formObject", "formLocation", "formChapter", "formDream", "formRelation", "formSymbol"].forEach((f) => {
+    document.getElementById(f).classList.add("hidden");
+  });
+  document.getElementById(id).classList.remove("hidden");
 }
 
 document.getElementById("openModal").addEventListener("click", function () {
-    document.getElementById("customModal").classList.remove("hidden");
+  document.getElementById("customModal").classList.remove("hidden");
 });
 
 document.getElementById("customModal").addEventListener("click", function (e) {
-    if (e.target === this) {
-        this.classList.add("hidden");
-    }
+  if (e.target === this) {
+    this.classList.add("hidden");
+  }
 });
