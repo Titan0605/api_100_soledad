d1fd5874654607838524df748e94e15fa9332b3c
Edwihn <a6523150063@utch.edu.mx>
2025-06-12 14:48:13 -0600

    Finish the modal of insert

--- app/static/javascript/validate_input.js
+++ app/static/javascript/validate_input.js
@@ -1,14 +1,76 @@
-  function filtrarCaracteres(input) {
-    input.value = input.value.replace(/[^A-Za-zÁÉÍÓÚáéíóúÑñ0-9\s]/g, '');
-  }
+function filtrarCaracteres(search) {
+  search.value = search.value.replace(/[^A-Za-zÑñ0-9\s]/g, '');
+}
 
-  function validarInput() {
-    const input = document.getElementById('to_search');
-    const pattern = /^[A-Za-zÑñ0-9\s]{1,70}$/;
 
-    if (!pattern.test(input.value)) {
-      alert("Por favor, escribe solo letras, números y espacios (máx. 50 caracteres).");
-      return false;
+//Validar para permitir comas
+function filtrarCaracteres1(input) {
+  input.value = input.value.replace(/[^A-Za-zÑñ,:\s]/g, '');
+}
+
+
+// Validar para solo texto
+function filtrarCaracteres2(input) {
+  input.value = input.value.replace(/[^A-Za-zÑñ\s]/g, '');
+}
+
+
+// Validar para ids
+function filtrarCaracteres3(input) {
+  input.value = input.value.replace(/[^A-Za-zÑñ0-9,\s]/g, '');
+}
+
+
+// Validar para varios cap
+function filtrarCaracteres4(input) {
+  input.value = input.value.replace(/[^0-9,\s]/g, '');
+}
+
+
+// Validar para inputs de 2
+const length_2 = [ 
+  document.getElementById('eventChapter'),
+  document.getElementById('eventImportance'),
+  document.getElementById('characterDeathChapter'),
+  document.getElementById('objectImportance'),
+  document.getElementById('chapterNumber'),
+  document.getElementById('chapterImportance'),
+  document.getElementById('dreamChapter'),
+  document.getElementById('relationStart'),
+  document.getElementById('relationEnd'),
+];
+
+length_2.forEach(input => {
+  input.addEventListener("input", () => {
+    if (input.value.length > 2) {
+      input.value = input.value.slice(0, 2);
     }
-    return true;
-  }
+  });
+});
+
+// Validar para inputs de 4
+const length_4 = [
+  document.getElementById('eventDate')
+];
+
+length_4.forEach(input => {
+  input.addEventListener("input", () => {
+    if (input.value.length > 4) {
+      input.value = input.value.slice(0, 4);
+    }
+  });
+});
+
+
+//Validar para inputs de 1
+const length_1 = [ 
+  document.getElementById('characterGeneration')
+];
+
+length_1.forEach(input => {
+  input.addEventListener("input", () => {
+    if (input.value.length > 1) {
+      input.value = input.value.slice(0, 1);
+    }
+  });
+});
